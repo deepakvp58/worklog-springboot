6188cf2f01a9adcff3322e826b06a7070b0baa6c
deepakvp58 <42162868+deepakvp58@users.noreply.github.com>
2021-02-10 11:41:02 +0530

    Config server commit

---
+++ config-server/src/main/groovy/com/gssp/configserver/ConfigServerApplication.groovy
@@ -0,0 +1,15 @@
+package com.gssp.configserver
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+class ConfigServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication, args)
+	}
+
+}
